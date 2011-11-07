42aeee96f3d82ca6ad5d9374de81cc0b76083468
Justin <justin.locsei@oberlin.edu>
2011-11-07 16:00:30 -0500

    Setup Cleanup
    Cleaned up the setup file.

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
 
-from setuptools import setup, find_packages
+try:
+	from setuptools import setup
+except ImportError:
+	from ez_setup import use_setuptools
+	use_setuptools()
+	from setuptools import setup
 
 setup(
 	name='djangoool',
 	version=__import__('djangoool').__version__,
-	description='A Django interface for working with ISO 639 languages.',
+	description='A Django interface to the pycountry module.',
 	author='Justin Locsei',
 	author_email='justin.locsei@oberlin.edu',
-	url='https://github.com/cilcoberlin/djangoool',
+	url='http://github.com/cilcoberlin/djangoool/',
 	download_url='https://github.com/cilcoberlin/djangoool/zipball/master',
-	packages=find_packages(),
-	package_data={'': ["*.*"]},
-	include_package_data=True,
-	zip_safe=False,
+	long_description=open('README.md', 'r').read(),
+	packages=[
+		'djangoool',
+		'djangoool.management',
+		'djangoool.management.commands'
+	],
+	requires=[
+		'pycountry'
+	],
+	install_requires=[
+		'pycountry'
+	],
 	classifiers=[
 		'Development Status :: 3 - Alpha',
 		'Environment :: Web Environment',
